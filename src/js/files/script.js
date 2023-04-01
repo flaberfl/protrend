f0d67bc351b58cb5a13c56b6379871c855762c81
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-04-02 00:33:05 +0300

    header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -84,3 +84,37 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
   });
 });
+
+let scrollpos = window.scrollY
+let lastScroll = 0;
+const scrollChange = 80
+const defaultOffset = 80;
+const add_class_on_scroll = () => header.classList.add("_header-bg")
+const remove_class_on_scroll = () => header.classList.remove("_header-bg")
+const header = document.querySelector('.header');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('_hide');
+
+window.addEventListener('scroll', () => {
+  if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+    //scroll down
+    header.classList.add('_hide');
+  } else if (scrollPosition() < lastScroll && containHide()) {
+    //scroll up
+    header.classList.remove('_hide');
+  }
+
+  lastScroll = scrollPosition();
+})
+
+window.addEventListener('scroll', function () {
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= scrollChange) {
+    add_class_on_scroll()
+  } else {
+    remove_class_on_scroll()
+  }
+
+})
