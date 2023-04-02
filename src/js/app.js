c01285c9f9c3a659c5a46ff884be751e120c50fd
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-04-02 11:38:10 +0300

    popup

--- src/js/app.js
+++ src/js/app.js
@@ -76,11 +76,11 @@
 
   /* Работа с полями формы */
   /* Документация: https://template.fls.guru/template-docs/rabota-s-formami.html */
-  // flsForms.formFieldsInit({ viewPass: false });
+  flsForms.formFieldsInit({ viewPass: false });
 
   /* Oтправка формы */
   /* Документация: https://template.fls.guru/template-docs/rabota-s-formami.html */
-  // flsForms.formSubmit();
+  flsForms.formSubmit();
 
   /* Модуль формы "колличество" */
   // flsForms.formQuantity();
