0d1506bd42528a460e96bfe2582e3deebc2b363b
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-04-02 13:24:40 +0300

    home

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -171,10 +171,10 @@ function initSliders() {
 			modules: [Autoplay],
 			/*  
 			effect: 'fade', */
-			// autoplay: {
-			// 	delay: 800,
-			// 	disableOnInteraction: false,
-			// },
+			autoplay: {
+				delay: 800,
+				disableOnInteraction: false,
+			},
 
 			observer: true,
 			observeParents: true,
@@ -186,7 +186,7 @@ function initSliders() {
 
 			//touchRatio: 0,
 			//simulateTouch: false,
-			// loop: true,
+			loop: true,
 			//preloadImages: false,
 			//lazy: true,
 			// Dotts
