c210b825a589b9b5bacbf80ad335094cf7fdff65
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-04-07 01:32:59 +0300

    zap

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -340,9 +340,9 @@ function initQuiz() {
 // console.log(item);
 
 
+// <source src="https://web-freelancer.ru/audio/01.mp3" type="audio/mp3"></source>
 
 // Функционал аудио-плеера для прослушивания отзывов
-
 
 const audioPlayer = document.querySelector(".audio-player");
 const audio = new Audio(
@@ -432,3 +432,94 @@ function getTimeCodeFromNum(num) {
     seconds % 60
   ).padStart(2, 0)}`;
 }
+
+
+
+// const audioPlayer2 = document.querySelector(".audio-player2");
+// const audio2 = new Audio(
+//   "https://web-freelancer.ru/audio/01.mp3"
+// );
+
+
+// console.dir(audio);
+
+// audio2.addEventListener(
+//   "loadeddata",
+//   () => {
+//     audioPlayer.querySelector(".time .length").textContent = getTimeCodeFromNum2(
+//       audio2.duration
+//     );
+//     audio2.volume = .75;
+//   },
+//   false
+// );
+
+// //click on timeline to skip around
+// const timeline2 = audioPlayer.querySelector(".timeline");
+// timeline.addEventListener("click", e => {
+//   const timelineWidth = window.getComputedStyle(timeline).width;
+//   const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+//   audio.currentTime = timeToSeek;
+// }, false);
+
+// //click volume slider to change volume
+// const volumeSlider2 = audioPlayer.querySelector(".controls .volume-slider");
+// volumeSlider.addEventListener('click', e => {
+//   const sliderWidth = window.getComputedStyle(volumeSlider).width;
+//   const newVolume = e.offsetX / parseInt(sliderWidth);
+//   audio.volume = newVolume;
+//   audioPlayer.querySelector(".controls .volume-percentage").style.width = newVolume * 100 + '%';
+// }, false)
+
+// //check audio percentage and update time accordingly
+// setInterval(() => {
+//   const progressBar = audioPlayer.querySelector(".progress");
+//   progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+//   audioPlayer.querySelector(".time .current").textContent = getTimeCodeFromNum2(
+//     audio.currentTime
+//   );
+// }, 500);
+
+// //toggle between playing and pausing on button click
+// const playBtn2 = audioPlayer.querySelector(".controls .toggle-play");
+// playBtn.addEventListener(
+//   "click",
+//   () => {
+//     if (audio.paused) {
+//       playBtn.classList.remove("play");
+//       playBtn.classList.add("pause");
+//       audio.play();
+//     } else {
+//       playBtn.classList.remove("pause");
+//       playBtn.classList.add("play");
+//       audio.pause();
+//     }
+//   },
+//   false
+// );
+
+// audioPlayer.querySelector(".volume-button").addEventListener("click", () => {
+//   const volumeEl = audioPlayer.querySelector(".volume-container .volume");
+//   audio.muted = !audio.muted;
+//   if (audio.muted) {
+//     volumeEl.classList.remove("icono-volumeMedium");
+//     volumeEl.classList.add("icono-volumeMute");
+//   } else {
+//     volumeEl.classList.add("icono-volumeMedium");
+//     volumeEl.classList.remove("icono-volumeMute");
+//   }
+// });
+
+// //turn 128 seconds into 2:08
+// function getTimeCodeFromNum2(num) {
+//   let seconds = parseInt(num);
+//   let minutes = parseInt(seconds / 60);
+//   seconds -= minutes * 60;
+//   const hours = parseInt(minutes / 60);
+//   minutes -= hours * 60;
+
+//   if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+//   return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+//     seconds % 60
+//   ).padStart(2, 0)}`;
+// }
