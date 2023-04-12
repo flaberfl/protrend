09e689bbc6627b32ebd3270b45517a1175a70de4
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-04-12 22:29:23 +0300

    menu body

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,33 +1,14 @@
-/*
-Документация по работе в шаблоне: 
-Документация слайдера: https://swiperjs.com/
-Сниппет(HTML): swiper
-*/
-
-// Подключаем слайдер Swiper из node_modules
-// При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
-// Пример: { Navigation, Autoplay }
 import Swiper, {
 	Autoplay,
 	Pagination
 } from 'swiper';
-/*
-Основниые модули слайдера:
-Navigation, Pagination, Autoplay, 
-EffectFade, Lazy, Manipulation
-Подробнее смотри https://swiperjs.com/
-*/
 
-// Стили Swiper
-// Базовые стили
 import "../../scss/base/swiper.scss";
 // Полный набор стилей из scss/libs/swiper.scss
 // import "../../scss/libs/swiper.scss";
 // Полный набор стилей из node_modules
 // import 'swiper/css';
 
-// Добавление классов слайдерам
-// swiper главному блоку, swiper-wrapper оболочке, swiper-slide для слайдов
 function bildSliders() {
 	//BildSlider
 	let sliders = document.querySelectorAll('[class*="__swiper"]:not(.swiper-wrapper)');
@@ -62,6 +43,7 @@ function initSliders() {
 				disableOnInteraction: false,
 			},
 			*/
+			effect: 'fade',
 			observer: true,
 			observeParents: true,
 			// slidesPerView: 'auto',
